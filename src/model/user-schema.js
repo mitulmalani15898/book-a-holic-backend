6510157905ef7cdb98d404daeede34983310ed3f
jainam <jn326778@dal.ca>
2022-03-25 13:30:41 -0300

    changes after new user schema

--- src/model/user-schema.js
+++ src/model/user-schema.js
@@ -7,9 +7,44 @@ const model = mongoose.model;
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
+  // firstName: String,
+  // lastName: String,
+  // email: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    default: null,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    default: null,
+    required: false,
+  },
+  preferences: {
+    type: String,
+    default: null,
+    required: false,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 autoincrement.initialize(mongoose.connection);

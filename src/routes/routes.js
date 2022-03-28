5ef892ad67e4452eea5b33608049265d1049c724
jainam <jn326778@dal.ca>
2022-03-28 00:43:49 -0300

    changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -17,12 +17,18 @@ const {
   deleteUser,
 } = require("../controller/user-controller");
 
-route.get("/api/user", getUsers);
-route.post("/api/user/add", addUser);
-route.get("/api/user/:id", getUserById);
-route.put("/api/user/:id", editUser);
-route.delete("/api/user/:id", deleteUser);
-
-//export default route;
+route.get("/api/users", getUsers);
+route.post("/api/users/add", addUser);
+route.get("/api/users/:id", getUserById);
+route.put("/api/users/edit/:id", editUser);
+route.delete("/api/users/delete/:id", deleteUser);
 
 module.exports = route;
+
+//route.get("/api/user", getUsers);
+//export default route;
+// route.get("/api/user/users", getUsers);
+// route.post("/api/user/add", addUser);
+// route.get("/api/user/:id", getUserById);
+// route.put("/api/user/:id", editUser);
+// route.delete("/api/user/:id", deleteUser);
